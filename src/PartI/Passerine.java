31ef9bb2ce1a7f1dcc793b168e474a3afc00bc3a
123R3N321 <jr5887@nyu.edu>
2025-03-06 17:43:57 -0500

    Finish Passerine class.

---
+++ src/PartI/Passerine.java
@@ -0,0 +1,47 @@
+/**
+ * extends bird
+ * songComplexity (double assumed)
+ * beakType (string assumed)
+ * single constructor
+ */
+package PartI;
+
+public abstract class Passerine extends Bird{
+    private double songComplexity;
+    private String beakType;
+
+    protected Passerine(String name, String scientificName, double lifeSpan, boolean isMobile, String dietType, boolean vertebrate, double nervousSystemComplexity, boolean canFly, double wingSpan, double songComplexity, String beakType) {
+        super(name, scientificName, lifeSpan, isMobile, dietType, vertebrate, nervousSystemComplexity, canFly, wingSpan);
+        this.songComplexity = songComplexity;
+        this.beakType = beakType;
+    }
+
+    public double getSongComplexity() {
+        return songComplexity;
+    }
+    public void setSongComplexity(double songComplexity) {
+        this.songComplexity = songComplexity;
+    }
+    public String getBeakType() {
+        return beakType;
+    }
+    public void setBeakType(String beakType) {
+        this.beakType = beakType;
+    }
+
+    //snow-balling methods
+    @Override
+    public void describe() {
+        super.describe();
+        System.out.print(" At Order level, this is a Passerine.");
+    }
+
+    @Override
+    public String getDetails() {
+        return super.getDetails() + ", and it has a song complexity of " +
+                Double.toString(songComplexity) +
+                " complexity units, and it has a beak type of " +
+                beakType;
+    }
+    //end of snow-balling methods
+}
